92af672223c8b120fd44d4bf0c0960578caafd12
Jeff Esp <jeff@todayisthursday.net>
2019-07-30 19:53:14 -0400

    Post and index generation done.

--- content/simple-sqlite-migration.md
+++ content/simple-sqlite-migration.md
@@ -6,7 +6,7 @@ Authors: Jeff Esp
 Summary: A simple SQLite migration script written in Python
 
 I had to write a migration for a SQLite database the other day, and decided
-to do some [yak shaving](1) and write the world's simplest db migration utility.
+to do some [yak shaving](https://en.wiktionary.org/wiki/yak_shaving) and write the world's simplest db migration utility.
 It only migrates one direction, and you have to write migration scripts by
 hand. But it seems to work, so here it is.
 
@@ -50,6 +50,3 @@ far.
 The `PRAGMA` stuff could be replaced by a table that keeps track of what the
 version, or even has a list of the scripts applied and some tracking data 
 about that. For now, `user_version` is going to work fine.
-
-
-[1]: https://en.wiktionary.org/wiki/yak_shaving
--- thursday.py
+++ thursday.py
@@ -1,31 +1,32 @@
 from datetime import datetime
 import os
 import markdown
+import shutil
 import urllib
+
 from markdown.extensions.codehilite import CodeHiliteExtension
 from jinja2 import Template, Environment, FileSystemLoader, select_autoescape
 from slugify import slugify
 
 
-class SiteContext(object):
-    pass
-
-
 class PostContext(object):
-    def __init__(self, title, date, content):
+    def __init__(self, base_dir, title, date, content):
+        self.base_dir = base_dir
         self.title = title
         self.date = datetime.strptime(date, '%Y-%m-%d')
         self.content = content
     
     def local_path(self):
-        return os.path.join(str(self.date.year), str(self.date.month), str(self.date.day), self.url_safe_title())
+        return os.path.join(self.base_dir, str(self.date.year), str(self.date.month), str(self.date.day), self.url_safe_title())
 
     def url_safe_title(self):
         return slugify(self.title) + ".html"
 
     def post_link_path(self):
-        # for now this should work
-        return self.local_path()
+        return f'/{self.date.year}/{self.date.month}/{self.date.day}/{self.url_safe_title()}'
+
+    def create_post_directory(self):
+        os.makedirs(os.path.dirname(self.local_path()), mode=0o755, exist_ok=True)
 
 
 class Thursday(object):
@@ -33,6 +34,7 @@ class Thursday(object):
         self.source_path='./content'
         self.template_path='./templates'
         self.output_path='./output'
+        self.static_path='./static'
 
         self.md = markdown.Markdown(extensions = ['meta', 'extra', CodeHiliteExtension()], output_format='html5', tab_length=2)
         self.jinja_env = Environment(
@@ -40,25 +42,47 @@ class Thursday(object):
             autoescape=select_autoescape(['html', 'xml'])
         )
 
-    def render_post(self, in_file, template, out_file):
+        self.rendered_posts = []
+
+        if os.path.exists(self.output_path):
+            shutil.rmtree(self.output_path)
+
+        if os.path.exists(self.static_path):
+            os.makedirs(os.path.join(self.output_path, self.static_path))
+            for f in os.listdir(self.static_path):
+                shutil.copy2(os.path.join(self.static_path, f), os.path.normpath(os.path.join(self.output_path , self.static_path, f)))
+
+    def render_post(self, in_file, template):
         try:
             html = self.md.convert(in_file.read())
-            pc = PostContext(self.md.Meta['title'][0], self.md.Meta['date'][0], html)
-            print(template.render(post_body=pc.content, title=pc.title, post_date=pc.date))
+            pc = PostContext(self.output_path, self.md.Meta['title'][0], self.md.Meta['date'][0], html)
+
+            pc.create_post_directory()
+            with open(pc.local_path(), "w") as f:
+                f.write(template.render(post_body=pc.content, title=pc.title, post_date=pc.date))
+
+            self.rendered_posts.append(pc)
         finally:
             self.md.reset()
 
-    def load_posts(self):
+    def write_posts(self):
         template = self.get_post_template(self.template_path)
         files = os.listdir(self.source_path)
+        # TODO: probably need to run through first, rather than render as I go 
         for f in files:
             with open(os.path.join(self.source_path, f)) as input_file:
-                self.render_post(input_file, template, None)
+                self.render_post(input_file, template)
 
-    def get_post_template(self, template_path):
-        return self.jinja_env.get_template('post.html')
+    def write_index(self):
+        template = self.get_post_template(self.template_path, 'index.html')
+        with open(os.path.join(self.output_path, 'index.html'), 'w') as f:
+            f.write(template.render(posts=self.rendered_posts))
+
+    def get_post_template(self, template_path, file='post.html'):
+        return self.jinja_env.get_template(file)
 
 
 if __name__ == "__main__":
     th = Thursday()
-    th.load_posts()
+    th.write_posts()
+    th.write_index()
